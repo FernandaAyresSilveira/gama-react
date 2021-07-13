b0a5018d071fafc543b156310f2e0c6856533e42
FernandaAyres Silveira <fernandaayressilveirah@gmail.com>
2021-07-13 15:36:27 -0300

    busca pelo usuario no github api

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App(props) {
+  const [usuario, setUsuario] = useState('');
+
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then( response => console.log(response.data));
+  }
+  return (
+
+    
+    <>
+    <p>{ usuario }</p>
+    <input name="usuario" id="usuario" value={usuario} className="usuarioInput" placeholder="Usuário" onChange={ e=>setUsuario(e.target.value)} />
+    <button type='button' onClick={handlePesquisa}>Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
+
+// [usuario, setUsuario]
